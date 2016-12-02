d439e1742c30345010905db0237a78c52e18cdb5
HL <hi.heyse@gmail.com>
2016-12-02 14:07:11 -0500

    Refactor

--- app/template.js
+++ app/template.js
@@ -1,15 +1,14 @@
-export default ({ body, title }) => {
-  return `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>${title}</title>
-      </head>
-      
-      <body>
-        <div id="root">${body}</div>
-      </body>
-      <script src="/assets/bundle.js"></script>
-    </html>
-  `;
-};
+export default ({ body, title }) => (`
+  <!DOCTYLE html>
+  <html>
+    <head>
+      <title>${title}</title>
+    </head>
+
+    <body>
+      <div id="root">${body}</div>
+    </body>
+
+    <script src="/assets/bundle.js"></script>
+  </html>
+`);
