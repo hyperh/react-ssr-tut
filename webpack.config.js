bc1457e101e03e49450ccc6e5672f4de33785bff
HL <hi.heyse@gmail.com>
2016-12-02 13:54:35 -0500

    Fix resolve

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
     ],
   },
   resolve: {
-    root: [path.join(__dirname, '..', 'app')],
+    root: [path.join(__dirname, '.', 'app')],
     extensions: ['', '.js', '.jsx'],
   },
 };
