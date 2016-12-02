dc88657166f6914b793f82f7e437043cc74c1c67
HL <hi.heyse@gmail.com>
2016-12-02 14:10:47 -0500

    Compile client side, get CSS working

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import webpack from 'webpack';
+import webpackConfig from '../webpack/webpack.config.dev-client';
 
 const render = require('../dist/assets/SSR');
 const app = express();
+
+const compiler = webpack(webpackConfig);
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: webpackConfig.output.publicPath,
+}));
 
 app.get('/', render.default);
 
